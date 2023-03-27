3a282567de3ddd2e9c42033bfd6dd0af6dee9160
creativerze <creativerze@gmail.com>
2023-03-27 12:00:37 +0600

    added menuHeader component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,10 +6,12 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
+import MenuHeader from "@/components/MenuHeader/MenuHeader";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <Header></Header>
+      <MenuHeader></MenuHeader>
       <Component {...pageProps} />
       <Footer></Footer>
     </Provider>
