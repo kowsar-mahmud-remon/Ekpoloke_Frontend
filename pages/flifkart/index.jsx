98f7e83bdfb6f19eb3cadc282b3e10055852f2d3
kopihannan <kopihannan3@gmail.com>
2023-04-19 11:10:53 +0600

    up

--- pages/flifkart/index.jsx
+++ pages/flifkart/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HomeNav from "../../components/FlipKart/HomeNav";
 import FlipKartCarousel from "../../components/FlipKart/FlipKartCarousel";
+import Products from "../../components/FlipKart/Products/Products"
 
 const index = () => {
   return (
     <div>
       <HomeNav></HomeNav>
       <FlipKartCarousel></FlipKartCarousel>
+      <Products></Products>
     </div>
   );
 };
