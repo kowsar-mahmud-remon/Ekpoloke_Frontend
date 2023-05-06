f7f51ae7b321112a65c7b3fa31325b9548a2c6b2
creativerze <creativerze@gmail.com>
2023-05-06 11:30:52 +0600

    solved Filter component error

--- components/Filter/Filter.tsx
+++ components/Filter/Filter.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { IoFilterOutline } from "react-icons/io5";
 
-const Filter = ({ className }) => {
+const Filter = ({ className }: any) => {
   return (
     <div
       className={`flex items-center justify-center border-[1px] border-gray-200 py-3 w-full ${className}`}
