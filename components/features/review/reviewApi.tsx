d678098486133ed9ec52165e049ea717c46ba8f6
creativerze <creativerze@gmail.com>
2023-05-07 14:31:40 +0600

    solved RatingModal component error

--- components/features/review/reviewApi.tsx
+++ components/features/review/reviewApi.tsx
@@ -1,16 +1,16 @@
 import { apiSlice } from "../api/apiSlice";
 
 const reviewApi = apiSlice.injectEndpoints({
-    endpoints: (build) =>({
-        addReview: build.mutation({
-            query: ({ productId, data, params }) => ({
-              url: `/api/rateProduct/${productId}`,
-              params,
-              method: "POST",
-              body: data,
-            }),
-          }),
-    })
-})
+  endpoints: (build) => ({
+    addReview: build.mutation({
+      query: ({ productId, data, params }) => ({
+        url: `/api/rateProduct/${productId}`,
+        params,
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
 
-export const {useAddReviewMutation} = reviewApi;
+export const { useAddReviewMutation } = reviewApi;
